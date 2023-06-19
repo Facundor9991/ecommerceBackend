1576a2270161b5d86d2f963dc9df42de41e9d3d3
Facundor9991 <facundor99@outlook.com>
2023-06-19 16:57:51 -0300

    entrega de desafio

---
+++ src/app.js
@@ -0,0 +1,47 @@
+import express from "express"
+
+
+const app = express()
+app.use(express.json())
+
+let usuarios = []
+
+app.get('/api/usuarios', (req, res) => {
+    res.json(usuarios)
+})
+
+app.post('/api/usuarios', (req, res) => {
+    const usuario = req.body
+
+    usuarios.push(usuario)
+    res.status(201).json({status: 'confirmado', mensaje: 'usuario creado'})
+})
+
+app.put('/api/usuarios/:id', (req, res) => {
+    const id = parseInt(req.params.id) 
+    const usuario = req.body
+
+    const usuarioIdx = usuarios.findIndex(u => u.id === id)
+    if(usuarioIdx <0){
+        return res.status(404).json({status: "error", mensaje: "Usuario no encontrado"})
+    }
+
+    usuarios[usuarioIdx] = usuario
+
+
+    res.json({status: "Aceptado", mensaje: "actualizando..."})
+})
+
+app.delete('/api/usuarios/:id', (req, res) =>{
+    const id = parseInt(req.params.id)
+
+    const usuarioIdx = usuarios.findIndex(u => u.id === id)
+    if(usuarioIdx <0){
+        return res.status(404).json({status: "error", mensaje: "Usuario no encontrado"})
+    }
+
+    usuarios = usuarios.filter(u => u.id !== id)
+    res.send({status: "Aceptado", mensaje: "Usuario eliminado con exito"})
+})
+
+app.listen(8080)
