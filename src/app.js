3a32c34bafcba99b0ebadcb04a10b729c371b33a
Facundor9991 <facundor99@outlook.com>
2023-07-22 17:41:43 -0300

    Actualizacion de proyecto

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ import ProductManager from "./manager/Eccomerce.js";
 
 //--------------------NUEVO 30/6
 
-import products from './routes/products.js'
-import cart from './routes/carts.js'
+import products from "./routes/products.js";
+import carts from "./routes/carts.js";
+import e from "express";
 //---------------------------
-
 
 const manager = new ProductManager("./src/productos.json");
 
@@ -14,6 +14,7 @@ const app = express();
 app.use(express.json());
 
 //-------------CODIGO DE PRUEBA------------
+/*
 app.get("/", async (req, res) => {
   const limit = parseInt(req.query.limit);
 
@@ -22,42 +23,48 @@ app.get("/", async (req, res) => {
   if (!limit) return res.json(products);
 
   if (limit) return res.json(products.slice(0, limit));
-});
+}); */
 
 app.get("/productos", async (req, res) => {
   const limit = parseInt(req.query.limit);
 
-  const products = await manager.getProducts();
+  try {
+    const products = await manager.getProducts();
 
-  if (!limit) return res.json(products);
-
-  if (limit) return res.json(products.slice(0, limit));
+    if (!limit) return res.json(products);
+    if (limit < -0)
+      return res.json({
+        message: "Este producto no existe",
+      });
+    if (limit) return res.json(products.slice(0, limit));
+  } catch (error) {
+    res.send(error);
+  }
 });
 
-app.get("/producto/:id", async (req, res) => {
-  const products = await manager.getProducts();
+app.get("/productos/:id", async (req, res) => {
+  try {
+    const products = await manager.getProducts();
+    const id = parseInt(req.params.id);
+    const product = products.find((element) => element.id === id);
 
-  const id = parseInt(req.params.id);
+    if (!products.some((element) => element.id === id))
+      return res.json({
+        id: id,
+        message: "El producto solicitado no esta disponible",
+      });
 
-  const product = products.find((element) => element.id === id);
-
-  if (!products.some((element) => element.id === id))
-    return res.json({
-      id: id,
-      message: "El producto solicitado no esta disponible",
-    });
-
-  return res.send(product);
+    return res.send(product);
+  } catch (error) {
+    res.send(error);
+  }
 });
-
 
 //--------------------NUEVO 30/6
-app.use('/api/products', products)
-app.use('/api/carts', cart)
+app.use("/api/products", products);
+app.use("/api/carts", carts);
 
 //--------------------
 
-
-
 app.listen(8080);
-console.log("De PrOnTo FlAsH");
+console.log("------EL PROGRAMA ESTA CORRIENDO------");
